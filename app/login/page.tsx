4a78044abc84cb28ba2a5ac96d3a8a731fbcd35c
Rajan Kumar Singh <rajankumarsinghvnit@gmail.com>
2025-08-03 23:09:25 +0530

    browser navigation

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,11 @@
 'use client';
 
-import { redirect } from 'next/navigation';
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://api.subgen.in';
 
-export default async function Login() {
-  redirect(`${API_BASE_URL}/login`);
-} 
+export default function Login() {
+  const handleLogin = () => {
+    window.location.href = `${API_BASE_URL}/login`;
+  };
+
+  return <button onClick={handleLogin}>Login</button>;
+}
